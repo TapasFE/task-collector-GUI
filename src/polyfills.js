745b44ab56e59d27fd2900859265fb05e1790150
Gerald <gera2ld@163.com>
2017-02-07 14:50:26 +0800

    fix: polyfill Object.assign

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,1 +1,18 @@
 import 'es6-promise';
+
+function polyfill(obj, key, value) {
+  if (!obj[key]) Object.defineProperty(obj, key, {
+    value,
+    configurable: true,
+    writable: true,
+  });
+}
+
+polyfill(Object, 'assign', (obj, ...args) => {
+  args.forEach(arg => {
+    arg && Object.keys(arg).forEach(key => {
+      obj[key] = arg[key];
+    });
+  });
+  return obj;
+});
